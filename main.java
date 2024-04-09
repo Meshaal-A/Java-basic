e5a73b8672690a25cadd2ea4da4f5c04c9808774
origin <mishalayubkhan10@gmail.com>
2024-04-09 13:39:46 +0530

    initial commit

---
+++ main.java
@@ -0,0 +1,18 @@
+/*public class main{
+    public static void main(String args[]){
+        for(int num=0;num<=5;num+=1){
+            System.out.println(num);
+        }
+    }
+}*/
+
+import java.util.Scanner;
+public  class main{
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        System.out.println("enter num");
+        int numr=num.nextInt();
+        System.out.println(numr);
+
+    }
+}
---
+++ primitives.java
@@ -0,0 +1,5 @@
+public class primitives {
+    public static void main(String[] args) {
+
+    }
+}
