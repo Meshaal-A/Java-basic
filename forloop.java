a27f7b7d3d2d31592ef472652e1343eeda38130b
origin <mishalayubkhan10@gmail.com>
2024-04-22 23:22:38 +0530

    new commit

--- forloop.java
+++ forloop.java
@@ -1,4 +1,4 @@
-public class forloop {
+public class forloop {//Executes a block of code repeatedly for a fixed number of times, or until a condition is met.
     public static void main(String[] args) {
         for (int num=1;num<=5;num++){
             System.out.println("num");
