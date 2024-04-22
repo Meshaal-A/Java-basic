90a6bf575f013212371173a956fbb7ba9221d7d8
origin <mishalayubkhan10@gmail.com>
2024-04-22 23:05:14 +0530

    new commit

---
+++ While.java
@@ -0,0 +1,13 @@
+public class While {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+        System.out.println(5);
+
+        int num=1;
+        while(num<=5);
+        System.out.println(num);
+    }
+}
