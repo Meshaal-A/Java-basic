d4f8e01c9ba789501514b99711ea5226315d45c2
origin <mishalayubkhan10@gmail.com>
2024-04-23 00:37:53 +0530

    new commit

---
+++ HelloClass.java
@@ -0,0 +1,13 @@
+public class HelloClass {
+    // Method to print a message
+    public void printHelloClass() {
+        System.out.println("Hello, I'm inside HelloClass!");
+    }
+
+    public static void main(String[] args) {
+        // Creating an instance of HelloClass
+        HelloClass helloObject = new HelloClass();
+        // Calling the printHelloClass method
+        helloObject.printHelloClass();
+    }
+}
