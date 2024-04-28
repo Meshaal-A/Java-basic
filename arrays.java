e121713e82faee4c0f234534e17dc0c71f2eb603
origin <mishalayubkhan10@gmail.com>
2024-04-28 07:12:12 +0530

    new commit

---
+++ arrays.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+public class arrays {
+    public static void main(String[] args) {
+
+        // Arrays(Type 2)
+        int[] marks = {98, 97, 95};
+        int[][] finalMarks={{98, 97, 95},{95, 97, 98}};
+        System.out.println(finalMarks[0][0]);
+
+        //Arrays(Type 1)
+
+//        int age=45;
+//        int eng=78;
+//        int math=88;
+//        int sci=89;
+//
+//
+//        int[] marks = new int[3];
+//        marks[0] = 97;
+//        marks[1] = 98;
+//        marks[2] = 95;
+//
+////      System.out.println(marks.length);
+//            Arrays.sort(marks);
+//        System.out.println(marks[1]);
+    }
+
+}
